fa43816416b409d2d1e13450ca80c45258c367d7
Tannic-Chalice <accforfullhacks@gmail.com>
2025-12-29 18:57:17 +0530

    Update flaskApi.ts

--- api/flaskApi.ts
+++ api/flaskApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // This MUST be localhost for your browser to reach the Docker container
-export const API_BASE_URL = "https://mlop-v1.onrender.com"; 
+export const API_BASE_URL = "mlop-production.up.railway.app"; 
 
 export const uploadImageForPrediction = async (file: File) => {
   const formData = new FormData();
@@ -21,3 +21,4 @@ export const uploadImageForPrediction = async (file: File) => {
   }
 
 };
+
